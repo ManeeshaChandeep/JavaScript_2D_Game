ca96e94aa28a83a6b2ea5ece647104898f6d2a10
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-10-03 00:29:20 +0530

    run_animation

--- src/script.js
+++ src/script.js
@@ -20,3 +20,37 @@ function idleAnimationStart() {
 idleAnimationNumber = setInterval(idleAnimation,200);
 
                               }
+    runAnimationNumber=0;
+    runImageNumber=0;
+
+    function runAnimation(){
+
+        runImageNumber = runImageNumber + 1;
+
+        if (runImageNumber==11){
+
+            runImageNumber=1;
+        }
+
+    boy.src="resources/sprites/png/Run ("+runImageNumber+").png";
+    }
+
+
+
+function  runAnimationStart(){
+        runAnimationNumber = setInterval(runAnimation,100);
+        clearInterval(idleAnimationNumber);
+    }
+
+    function keyCheck(event){
+// alert(event.which);
+// enter=13
+        var keyCode=event.which;
+        if(keyCode == 13){
+            if(runAnimationNumber == 0){
+                runAnimationStart();
+            }
+
+        }
+
+    }
