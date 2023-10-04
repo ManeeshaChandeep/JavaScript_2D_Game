056e4fba96a878fbe5341aa7a8d74dcd4811ea01
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-10-04 19:42:07 +0530

    jump_animation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 var boy=document.getElementById("boy");
 
-    idleImageNumber=0;
+    idleImageNumber=1;
     idleAnimationNumber=0;
 
 function idleAnimation(){
@@ -21,7 +21,7 @@ idleAnimationNumber = setInterval(idleAnimation,200);
 
                               }
     runAnimationNumber=0;
-    runImageNumber=0;
+    runImageNumber=1;
 
     function runAnimation(){
 
@@ -41,22 +41,73 @@ function  runAnimationStart(){
         runAnimationNumber = setInterval(runAnimation,100);
         clearInterval(idleAnimationNumber);
     }
+jumpImageNumber=1;
+jumpAnimationNumber=0;
+boyMarginTop = 410;
+
+    function jumpAnimation(){
+        jumpImageNumber=jumpImageNumber+1;
+
+        if(jumpImageNumber <= 6){
+            boyMarginTop = boyMarginTop - 30 ;
+            boy.style.marginTop = boyMarginTop + "px";
+        }
+        if(jumpImageNumber >=7 ){
+            boyMarginTop = boyMarginTop + 30 ;
+            boy.style.marginTop = boyMarginTop + "px";
+        }
+
+        if(jumpImageNumber==11){
+            jumpImageNumber=1;
+            clearInterval(jumpAnimationNumber);
+            jumpAnimationNumber =0;
+            runImageNumber = 0;
+            runAnimationStart();
+
+        }
+
+        boy.src="resources/sprites/png/Jump ("+jumpImageNumber+").png";
+
+    }
+function jumpAnimationStart(){
+        clearInterval(idleAnimationNumber);
+        runImageNumber = 0;
+        clearInterval(runAnimationNumber);
+        jumpAnimationNumber = setInterval(jumpAnimation,100);
+}
+
+
 
     function keyCheck(event){
 // alert(event.which);
 // enter=13
+        //space=32
         var keyCode=event.which;
         if(keyCode == 13){
             if(runAnimationNumber == 0){
                 runAnimationStart();
             }
 
-        }
+
 
         if(moveBackgroundAnimationId==0){
             moveBackgroundAnimationId= setInterval(moveBackground,100);
 
         }
+
+        }
+
+        if(keyCode==32){
+            if (jumpAnimationNumber==0){
+                jumpAnimationStart();
+            }
+            if(moveBackgroundAnimationId==0){
+                moveBackgroundAnimationId= setInterval(moveBackground,100);
+
+            }
+
+        }
+
 
 
 
